7022a15dfdec1c934fe285db8fdd4351f1eb4488
Adombang Munang Mbomndih <dzedock@yahoo.com>
2018-05-17 05:16:18 +0300

    Complete react frontend

--- public/components/app.js
+++ public/components/app.js
@@ -1,14 +1,40 @@
-import React, { Fragment } from 'react';
-import Status from './status';
-import Card from './card';
+import React, {Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import initialize from './initialize';
 
-const App = () => {
-    return (
-        <Fragment>
-            <Status />
-            <Card />
-        </Fragment>
-    );
-};
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            numberOfPlayers: 0,
+            score: 0,
+            text: 'Wait for next round...',
+            time: 5
+        };
+    }
+
+    componentDidMount(){
+        initialize(this);
+    }
+    
+    render(){
+        return (
+            <Fragment>
+                <div>
+                    <span id="numPlayers">Players: {this.state.numberOfPlayers}</span>
+                    <span id="score">Score: {this.state.score}</span>
+                </div>
+                <div id="questionCard">
+                    <p id="textPad">{this.state.text}</p>
+                    <span id="timer">{this.state.time}</span>
+                    <div id="buttonHolder">
+                        <button id="yesButton">Yes</button>
+                        <button id="noButton">No</button>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
 
 export default App;
---
+++ public/components/initialize.js
@@ -0,0 +1,123 @@
+function initialize(self) {
+	'use strict';
+
+	let utils = new Utils(),
+		sock = new SocketIO(self);
+
+	// Window Click Listeners
+	window.addEventListener('click', event => {
+		let element = event.target;
+		
+		if (element.id === 'scrollToTop') Q('header').scrollIntoView({
+			behavior:'smooth',
+			block: 'start'
+		});
+		else if(element.id === 'yesButton') sock.handleAnswer('yes');
+		else if(element.id === 'noButton') sock.handleAnswer('no');
+	});
+
+	// Set Default States/Values
+	I('copy').appendChild(document.createTextNode(' - '+new Date().getFullYear()));
+	
+}
+
+function SocketIO(self) {
+	'use strict';
+
+	if (!this || !(this instanceof SocketIO)) return new SocketIO();
+
+	let socket = io(),
+		utils = new Utils(),
+		currentResult,
+		correctAnswer = false,
+		roundClosed = false,
+		waitInterval,
+		roundInterval;
+
+	socket.on('connection', utils.success('Welcome'));
+	socket.on('question', payload => handleQuestion(payload));
+	socket.on('round closed', payload => handleRoundClosed(payload));
+	socket.on('player update', payload => self.setState({ numberOfPlayers: payload }));
+	
+	// Update UI with new question
+	function handleQuestion(payload)
+	{
+		let count = 10; // Each round last for 10 seconds
+
+		clearInterval(waitInterval);
+		roundClosed = false;
+		correctAnswer = false;
+		currentResult = payload.result;
+		self.setState({ time: count-- });
+
+		// Show a countdown of the game time (10s)
+		roundInterval = setInterval(() => {
+			if (count === 0) return clearInterval(roundInterval);
+			self.setState({	time: count-- });
+		}, 1000);
+
+		// Display the question and response buttons
+		self.setState({
+			text: `${payload.operand1} ${payload.operator}  ${payload.operand2} = ${payload.answer}`
+		});
+		I('buttonHolder').style.display = 'flex';
+	}
+	
+	// Update UI when round ends
+	function handleRoundClosed(payload)
+	{
+		if (correctAnswer) utils.success('correct :)');
+		else utils.warn('Round closed!');
+		waitForNextRound();
+	}
+
+	// Handle players response
+	function handleAnswer(answer)
+	{
+		// If the round has ended, notify the player and do nothing else
+		if (roundClosed) return utils.warn('This round is closed!');
+
+		let newScore;
+			
+		// Update player's score on UI base on his/her response
+		if (answer === currentResult){
+			newScore = ++self.state.score;
+			// If player's response is correct, notify the server so that
+			// next round can begin
+			socket.emit('answer', answer);
+			correctAnswer = true;
+		}
+		else{
+			newScore = self.state.score > 0 ? --self.state.score : 0;
+			utils.failure('wrong :(');
+			resetView();
+		}
+
+		self.setState({ time: newScore });
+	}
+
+	function waitForNextRound() {
+		let count = 5; // Delay of 5 seconds between rounds
+
+		clearInterval(roundInterval);
+		roundClosed = true;
+		self.setState({ time: count-- });
+		resetView();
+
+		waitInterval = setInterval(() => {
+			if (count === 0) return clearInterval(waitInterval);
+			self.setState({	time: count-- });
+		}, 1000);
+	}
+
+	function resetView() {
+		self.setState({ text: 'Wait for next round...' });
+		I('buttonHolder').style.display = 'none';
+	}
+	
+	return {
+		handleAnswer
+	};
+}
+
+export default initialize;
